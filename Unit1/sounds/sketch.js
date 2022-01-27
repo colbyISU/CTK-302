798ba5cdd45a61920c185be0157f81f914da0d34
colbyISU <jdcolby@ilstu.edu>
2022-01-27 11:10:08 -0600

    Sound sketch.js.

--- Unit1/sounds/sketch.js
+++ Unit1/sounds/sketch.js
@@ -1,7 +1,26 @@
+let song1;
+
+
+function preload() {
+  song1 = loadSound("assets/CreativeMinds.mp3");
+}
 function setup() {
   createCanvas(500, 500);
+  song1.loop();
 }
 
 function draw() {
+  background(100);
+}
 
+function mouseReleased() {
+  if (song1.isPlaying()) {
+    song1.pause();
+  } else {
+    song1.loop();
+  }
+}
+
+function touchStarted() {
+  getAudioContext().resume();
 }
